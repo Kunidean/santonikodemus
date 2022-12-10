891e1c35f168e6f1a4ae22d0b5819470b8a0d1fe
Meki D. YaoYan <michael.danda@student.umn.ac.id>
2022-12-10 16:52:14 +0700

    fix: fixing the codes

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,10 +3,10 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- @vite('resources/css/app.css')
-  @vite('resources/css/font.css')
-  @vite('resources/css/gambar.css')
-  @vite(['resources/css/app.css', 'resources/js/app.js']) -->
+  <!-- @vite('resources/css/app.css') -->
+  <!-- @vite('resources/css/font.css') -->
+  <!-- @vite('resources/css/gambar.css') -->
+  <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
 
           <!-- CDN -->
           <script src="https://cdn.tailwindcss.com"></script>
