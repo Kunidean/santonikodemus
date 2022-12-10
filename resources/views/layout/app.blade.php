60cc7d3249c8cfca95c4f570a916a8ed2cc1738e
Meki D. YaoYan <michael.danda@student.umn.ac.id>
2022-12-10 17:19:22 +0700

    fix: fixing the codes

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
     <!-- @vite('resources/css/app.css') -->
     <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
               <!-- CDN -->
