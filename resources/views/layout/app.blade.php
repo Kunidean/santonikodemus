32808ee4ad1757754cc381775b89ddf745d801d9
Meki D. YaoYan <michael.danda@student.umn.ac.id>
2022-12-20 15:46:50 +0700

    Update app.blade.php

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -51,20 +51,22 @@
   </style>
   </head>
 <body>
-<div class="w-full flex align-center py-[0.5em] px-[3em] lg:px-[4em] xl:px-[8em] text-white bg-[#01B7C8]">
+<div class="w-full flex align-center justify-between lg:justify-start py-[0.5em] px-[3em] lg:px-[4em] xl:px-[8em] text-white bg-[#01B7C8]">
     <div class="flex">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
         <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z" clip-rule="evenodd" />
         </svg>
         <div class="ml-2 hidden md:flex"><a>+6221-7429616</a></div>
     </div>
+    <a href="mailto:santonikodemus.ciputat@gmail.com">
     <div class="flex ml-[2.5em]">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
       <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
       <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
     </svg>
-        <div class="ml-2 hidden md:flex"><a href="mailto:santonikodemus.ciputat@gmail.com">santonikodemus.ciputat@gmail.com</a></div>
+        <div class="ml-2 hidden md:flex">santonikodemus.ciputat@gmail.com</div>
     </div>
+    </a>
 </div>
 <nav class="
   px-[1em]
@@ -84,7 +86,6 @@
   <div class="container-fluid w-full flex items-center justify-between px-6">
   <div class="flex justify-between items-center" id="navbarSupportedContent">
   <a class="
-        hidden
         xl:flex
         items-center
         text-white
