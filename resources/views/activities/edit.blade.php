809bdfc5653be8c2c926b38fd7fd608e352cfdbd
Meki D. YaoYan <michael.danda@student.umn.ac.id>
2022-12-20 15:54:10 +0700

    fix: @vite

--- resources/views/activities/edit.blade.php
+++ resources/views/activities/edit.blade.php
@@ -4,10 +4,10 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
-  @vite('resources/css/font.css')
-  @vite('resources/css/gambar.css')
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  <link rel="preconnect" href="resources/css/app.css">
+  <link rel="preconnect" href="resources/css/font.css">
+  <link rel="preconnect" href="resources/css/gambar.css">
+  <link rel="preconnect" href="resources/js/app.js">
   <script src="./TW-ELEMENTS-PATH/dist/js/index.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
